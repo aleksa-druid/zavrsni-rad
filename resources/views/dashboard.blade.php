5f7d49803622713362ac9ff601ee80376d7f93a3
aleksa-druid <bosko.alexandrovic@gmail.com>
2025-09-24 20:13:14 +0200

    added logout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,11 @@
 <x-layouts.app>
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+
+        <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            {{ __('Log Out') }}
+        </button>
+    </form>
     <div class="h-screen flex">
         <div class="w-1/4 border-r">
             <livewire:contacts-list />
